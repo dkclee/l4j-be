30e3cef5c9bf06ef4f82904d4915699262a6fedf
David Lee <davidlee980804@gmail.com>
2021-01-13 10:51:30 -0800

    Finished writing tests for helper/sql.js

--- helpers/sql.test.js
+++ helpers/sql.test.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const { sqlForPartialUpdate } = require("./sql");
+const { BadRequestError } = require("../expressError");
 
 /* Test for sqlForPartialUpdate */
 
@@ -13,9 +14,28 @@ describe("sqlForPartialUpdate", function () {
 
     expect(setCols).toEqual('"first_name"=$1, "age"=$2');
     expect(values).toEqual(["test", 3]);
-    
   });
 
+  test("works even if jsToSql is empty", function () {
+    const dataToUpdate = {last_name: "test2", age: 15};
+    const jsToSql = {};
 
+    const { setCols, values } = sqlForPartialUpdate(dataToUpdate, jsToSql); 
+
+    expect(setCols).toEqual('"last_name"=$1, "age"=$2');
+    expect(values).toEqual(["test2", 15]);
+  });
+
+  test("BadRequestError if dataToUpdate is empty", function () {
+    const dataToUpdate = {};
+    const jsToSql = {firstName: "first_name"};
+
+    try {
+      const { setCols, values } = sqlForPartialUpdate(dataToUpdate, jsToSql); 
+      fail();
+    } catch (err) {
+      expect(err instanceof BadRequestError).toBeTruthy();
+    }
+  });
   
 });
